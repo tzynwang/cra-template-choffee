086ad7ccc51bc5139bff87ab8a18cd24e8ad9a1e
Charlie <charlie@lawsnote.com>
2022-09-05 15:26:53 +0800

    [feat] ErrorBoundary lint

--- template/src/components/Common/ErrorBoundary/index.tsx
+++ template/src/components/Common/ErrorBoundary/index.tsx
@@ -1,47 +1,50 @@
-import React, { memo } from 'react'
-import classNames from 'classnames'
-
-import Styles from  './index.module.css'
-import { Props, State } from './types'
+import React, { memo } from 'react';
+import cn from 'classnames';
+import scopedStyle from './index.module.css';
+import type { Props, State } from './types';
 
 const INITIAL_STATE: State = {
   hasError: false,
   errorMessage: null,
-  callStack: null
-}
+  callStack: null,
+};
 
 class ErrorBoundary extends React.Component<Props, State> {
-  state: State = INITIAL_STATE
+  state: State = INITIAL_STATE;
 
   static getDerivedStateFromError(error: Error) {
-    const errorMessage = error.message ? error.message : String(error)
+    const errorMessage = error.message ? error.message : String(error);
 
     const callStack = error.stack
       ? error.stack.split('\n').slice(1).join('\n')
-      : null
+      : null;
 
     return {
       callStack,
       errorMessage,
-      hasError: true
-    }
+      hasError: true,
+    };
   }
 
   render() {
-    const { children } = this.props
-    const { hasError, errorMessage, callStack } = this.state
+    const { children } = this.props;
+    const { hasError, errorMessage, callStack } = this.state;
 
     if (hasError) {
       return (
         <div>
-          <div className={classNames(Styles.errorBoundaryTitle)}>Oops: {errorMessage}</div>
-          <pre className={classNames(Styles.errorBoundaryMessage)}>{callStack}</pre>
+          <div className={cn(scopedStyle.errorBoundaryTitle)}>
+            Oops: {errorMessage}
+          </div>
+          <pre className={cn(scopedStyle.errorBoundaryMessage)}>
+            {callStack}
+          </pre>
         </div>
-      )
+      );
     }
 
-    return children
+    return children;
   }
 }
 
-export default memo(ErrorBoundary)
+export default memo(ErrorBoundary);
